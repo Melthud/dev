9d21652d565c989cae6a8312e9988aefe98b9970
Melthud <49351054+Melthud@users.noreply.github.com>
2023-12-07 08:58:57 -0500

    otra

--- app/dashboard/ventas/[id]/edit/not-found.tsx
+++ app/dashboard/ventas/[id]/edit/not-found.tsx
@@ -5,8 +5,8 @@ export default function NotFound() {
   return (
     <main className="flex h-full flex-col items-center justify-center gap-2">
       <FaceFrownIcon className="w-10 text-gray-400" />
-      <h2 className="text-xl font-semibold">404 Not Found</h2>
-      <p>No se encontro la peticion.</p>
+      <h2 className="text-xl font-semibold">Lo sentimos no estas registrado.</h2>
+      <p>Pide tus credencales en gerencia de JYS.</p>
       <Link
         href="/dashboard/invoices"
         className="mt-4 rounded-md bg-blue-500 px-4 py-2 text-sm text-white transition-colors hover:bg-blue-400"
